66afe4e7cbf793a745ff072884239eab4d05cdfe
Luis Gustavo <luisgustavoolivtp@hotmail.com>
2022-08-07 21:07:49 -0300

    Novas estilizações, useEffect e listagem de componentes pelo map

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,10 +1,10 @@
 export function RepositoryItem(props){
     return(
         <li>
-            <strong>{props.repository}</strong>
-            <p>Forms in React</p>
+            <strong>{props.repository.name}</strong>
+            <p>{props.repository.description}</p>
 
-            <a href="https://www.youtube.com/">
+            <a href={props.repository.html_url}>
                 Acessar repositório
             </a>
         </li>
